5ea93d17db02e71cd172a67cde26a1b5b583a58d
José Romualdo <joseromualdo@outlook.com>
2022-04-12 19:19:20 -0300

    add componete

--- src/app/shared/nova-tarefa/nova-tarefa.component.ts
+++ src/app/shared/nova-tarefa/nova-tarefa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-nova-tarefa',
@@ -7,24 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaTarefaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController, 
+              public toastController: ToastController) {}
 
 
   nomeTarefa: String = '';
 
   listaDeTarefas: Array<String> = [];
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  async presentAlert(titulo: string, subtitulo: string, message: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: titulo,
+      subHeader: subtitulo,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Sua tarefa foi salva.',
+      duration: 2000
+    });
+    toast.present();
+  }
 
   //1 - Ter uma lista p/ armazenar as tarefas
   //2 - Ler a tarefa atual e adicionar na lista
   //3 - Limpar a tarefa a tarefa atual
 
-  addNovaTarefa(): void{
-   // alert(this.nomeTarefa);
+  addNovaTarefa(): void {
+    if (this.nomeTarefa == '') {
+     
+     this.presentAlert('Atenção', '', 'Informe o nome da tarefa');
+      // alert('Informe o nome da tarefa');
+      return;
+    }
 
-   this.listaDeTarefas.push(this.nomeTarefa);
-  this.nomeTarefa = '';
+    this.listaDeTarefas.push(this.nomeTarefa);
+    this.nomeTarefa = '';
+    this.presentToast();
   }
 
 }
